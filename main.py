6d16474c9080a32b9b10a06c94e12381f90ebd23
thtr-code <tt7uong@gmail.com>
2025-12-28 18:08:25 -0800

    Plot 3% Vs NRtg and 3PA vs NRtg using Matplotlib subplots and scatter plots

--- main.py
+++ main.py
@@ -10,42 +10,41 @@ Problem Definition: Is NBA (2024-2025) team success (wins/net rating) more stron
 player_df = pd.read_csv('player_data.csv')
 team_df = pd.read_csv('team_data.csv')
 
-
 team_map = {
-    'ATL': 'Atlanta Hawks',
-    'BOS': 'Boston Celtics',
-    'BRK': 'Brooklyn Nets',
-    'CHI': 'Chicago Bulls',
-    'CHO': 'Charlotte Hornets',
-    'CLE': 'Cleveland Cavaliers',
-    'DAL': 'Dallas Mavericks',
-    'DEN': 'Denver Nuggets',
-    'DET': 'Detroit Pistons',
-    'GSW': 'Golden State Warriors',
-    'HOU': 'Houston Rockets',
-    'IND': 'Indiana Pacers',
-    'LAC': 'Los Angeles Clippers',
-    'LAL': 'Los Angeles Lakers',
-    'MEM': 'Memphis Grizzlies',
-    'MIA': 'Miami Heat',
-    'MIL': 'Milwaukee Bucks',
-    'MIN': 'Minnesota Timberwolves',
-    'NOP': 'New Orleans Pelicans',
-    'NYK': 'New York Knicks',
-    'OKC': 'Oklahoma City Thunder',
-    'ORL': 'Orlando Magic',
-    'PHI': 'Philadelphia 76ers',
-    'PHO': 'Phoenix Suns',
-    'POR': 'Portland Trail Blazers',
-    'SAC': 'Sacramento Kings',
-    'SAS': 'San Antonio Spurs',
-    'TOR': 'Toronto Raptors',
-    'UTA': 'Utah Jazz',
-    'WAS': 'Washington Wizards'
+    'Atlanta Hawks': 'ATL',
+    'Boston Celtics': 'BOS',
+    'Brooklyn Nets': 'BRK',
+    'Chicago Bulls': 'CHI',
+    'Charlotte Hornets': 'CHO',
+    'Cleveland Cavaliers': 'CLE',
+    'Dallas Mavericks': 'DAL',
+    'Denver Nuggets': 'DEN',
+    'Detroit Pistons': 'DET',
+    'Golden State Warriors': 'GSW',
+    'Houston Rockets': 'HOU',
+    'Indiana Pacers': 'IND',
+    'Los Angeles Clippers': 'LAC',
+    'Los Angeles Lakers': 'LAL',
+    'Memphis Grizzlies': 'MEM',
+    'Miami Heat': 'MIA',
+    'Milwaukee Bucks': 'MIL',
+    'Minnesota Timberwolves': 'MIN',
+    'New Orleans Pelicans': 'NOP',
+    'New York Knicks': 'NYK',
+    'Oklahoma City Thunder': 'OKC',
+    'Orlando Magic': 'ORL',
+    'Philadelphia 76ers': 'PHI',
+    'Phoenix Suns': 'PHO',
+    'Portland Trail Blazers': 'POR',
+    'Sacramento Kings': 'SAC',
+    'San Antonio Spurs': 'SAS',
+    'Toronto Raptors': 'TOR',
+    'Utah Jazz': 'UTA',
+    'Washington Wizards': 'WAS'
 }
 
 player_df = player_df[~player_df['Team'].isin(['2TM', '3TM'])]
-player_df['Team'] = player_df['Team'].map(team_map)
+team_df['Team'] = team_df['Team'].map(team_map)
 
 player_df['3PA_total'] = player_df['3PA'] * player_df['G'] / 82
 player_df['3P_total'] = (player_df['3P'] * player_df['G']) / 82
@@ -64,15 +63,30 @@ final_df['3P_PCT'] = final_df['3P_total'] / final_df['3PA_total']
 
 final_df = final_df.sort_values('Rk', inplace=False, ascending=True)
 final_df = final_df.set_index('Rk')
-print(final_df)
+print(final_df) ## In hindsight, probably didn't need team win loss
 
-'''
-Playing around with diff funcs ignore 
 
-print(player_df[player_df['3P'] == player_df['3P'].max()].to_string())
-print(player_df.loc[player_df['3P'].idxmax(),'Player'])
-print(final_df[['W', 'L','NRtg','3P_total', '3PA_total', '3P_PCT']])
+figure, axes = plt.subplots(2,1)
 
-'''
+## 3% vs Net Rating
+
+axes[0].scatter(final_df['3P_PCT'], final_df['NRtg'])
+axes[0].set_xlabel('3P%')
+axes[0].set_ylabel('Net Rating')
+
+##3PA vs Net Rating
+axes[1].scatter(final_df['3PA_total'], final_df['NRtg'])
+axes[1].set_xlabel('3P Attempted')
+axes[1].set_ylabel('Net Rating')
+
+
+for team, x, y in zip(final_df['Team'], final_df['3P_PCT'], final_df['NRtg']):
+    axes[0].annotate(team, (x, y), fontsize=8)
+for team, x, y in zip(final_df['Team'], final_df['3PA_total'], final_df['NRtg']):
+    axes[1].annotate(team, (x, y), fontsize=8)
+plt.tight_layout()
+plt.show()
+
+
 
 
